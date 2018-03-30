20ec114c1644d360c6d2a5790663af736dc9b2f0
Umair Ahmed <uahmed@memphis.edu>
2018-03-30 01:43:17 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
+        document.getElementById("user_div").style.display = "block";
+        document.getElementById("login_div").style.display = "none";
+    } else {
+
+         document.getElementById("user_div").style.display = "none";
+        document.getElementById("login_div").style.display = "block";
+        // No user is signed in.
+    }
+});
+
+
+function login(){
+    
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+    
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        window.alert("Error : " + errorCode + " " + errorMessage);
+    });
+    
+    
+}
+
+function logout(){
+    
+    firebase.auth().signOut()                                   
+    
+}
