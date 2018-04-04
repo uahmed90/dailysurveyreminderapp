68578d439d78f83459bfecbdb5ac3e20392a9624
Shane Gladden <sgladden@memphis.edu>
2018-04-03 23:55:42 -0500

    It works

--- home.js
+++ home.js
@@ -1,4 +1,18 @@
 
+
+
+var user = firebase.auth().currentUser;
+
+if(user != null){
+
+	var email_id = user.email;
+	document.getElementById("user_para").innerHTML="Welcome " + email_id;
+
+
+}
+
+
+
 function signOut(){
 	firebase.auth().signOut().then(function() {
 	  // Sign-out successful.
