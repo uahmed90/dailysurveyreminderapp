7c7754e6bcae739fef8dcf238afd8471153f4849
Shane Gladden <sgladden@memphis.edu>
2018-04-03 22:19:23 -0500

    F someone else's L

--- home.js
+++ home.js
@@ -1,21 +1,4 @@
 
-
-var user = firebase.auth().currentUser;
-
-if (user) {
-  // User is signed in.
-  document.getElementById("user_div").style.display = "block";
-  if (user != null){
-            var email_id = user.email;
-			document.getElementById("user_para").innerHTML = "Welcome "+ email_id;
-        }
-
-} else {
-  // No user is signed in.
-  location.href = 'index.html';
-}
-
-
 function signOut(){
 	firebase.auth().signOut().then(function() {
 	  // Sign-out successful.
